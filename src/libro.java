3a11475147d10b9cca2d2d2af8a67b7d20f8b104
juanpaocampo45 <juanpabloocamporodriguez@gmail.com>
2026-05-22 18:34:35 -0500

    Update libro.java
    cambios hechos

--- src/libro.java
+++ src/libro.java
@@ -1,10 +1,23 @@
-public class Libro {
-    private String titulo;
- private String autor;
- private int numeroPaginas;
- private final String isbn;
- private static int contadorLibros = 0;
-uja
-    sfa
-    asdf
-    jjuyaSD
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Libro libro1 = new Libro(); // Usa constructor por defecto
+
+        Libro libro2 = new Libro(
+                "Cien años de soledad",
+                "Gabriel Garcia Márquez",
+                432,
+                "978-8437604947"
+        );
+
+        libro1.mostrarInfo();
+        libro2.mostrarInfo();
+
+        libro2.setNumeroPaginas(450); // Modifica un atributo
+
+        System.out.println("El titulo del libro2 es: " + libro2.getTitulo());
+
+        System.out.println("Total de libros creados: " + Libro.getTotalLibros());
+    }
+}
